40545c51e633abe30d8c4af1690f16591323bfcc
Panos Fykouras <panosfykouras@gmail.com>
2023-05-27 22:11:16 +0200

    Background and Install script

--- i3/install.sh
+++ i3/install.sh
@@ -1,10 +1,11 @@
 #1/bin/bash
-sudo
+
 mkdir ~/.config/i3
 cp config ~/.config/i3/.
 mkdir ~/.config/i3blocks/
 cp i3blocks.conf ~/.config/i3blocks/
-
+cd ..
+cp -r pictures/Background ~/Pictures/
 
 mkdir /home/$USER/scripts
 cp -r i3blocksScripts /home/$USER/scripts/
@@ -15,10 +16,14 @@ cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 echo "exec i3" >> ~/.xinitrc
 
-sudo mkdir /etc/sddm.conf.d/ 
-sudo touch autologin.conf
-sudo echo "[Autologin]" >> autologin.conf
-sudo echo "User=" >> autologin.conf
-sudo echo "Session=" >> autologin.conf
+sudo mkdir /etc/sddm.conf.d
+cd
+touch autologin.conf
+echo "[Autologin]" >> autologin.conf
+echo "User=" >> autologin.conf
+echo "Session=" >> autologin.conf
 sudo mv autologin.conf /etc/sddm.conf.d/
 sudo systemctl enable --now sddm
+echo " "
+echo " "
+echo "Please reboot the system."
