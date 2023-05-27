00e3fb313c1e1a2260e3935d8eeadb095c73cf20
Panos Fykouras <panosfykouras@gmail.com>
2023-05-27 21:17:14 +0200

    Install script update

--- i3/install.sh
+++ i3/install.sh
@@ -1,5 +1,5 @@
 #1/bin/bash
-
+sudo
 mkdir ~/.config/i3
 cp config ~/.config/i3/.
 mkdir ~/.config/i3blocks/
@@ -7,6 +7,18 @@ cp i3blocks.conf ~/.config/i3blocks/
 
 
 mkdir /home/$USER/scripts
-cp -r i3blocksScripts /home/$User/scripts/
+cp -r i3blocksScripts /home/$USER/scripts/
 
+sudo pacman -Syu $(cat packageList)
 
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+echo "exec i3" >> ~/.xinitrc
+
+sudo mkdir /etc/sddm.conf.d/ 
+sudo touch autologin.conf
+sudo echo "[Autologin]" >> autologin.conf
+sudo echo "User=" >> autologin.conf
+sudo echo "Session=" >> autologin.conf
+sudo mv autologin.conf /etc/sddm.conf.d/
+sudo systemctl enable --now sddm
